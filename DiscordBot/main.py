2182189595d41d500c81d9c945957c1c0afdd547
MKCube <kostka969@interia.pl>
2022-05-04 18:23:06 +0200

    First Program

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -14,7 +14,6 @@ async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-
     print(
         f"{client.user} has connected to Discord!"
         f"{client.user} is connected to the following guild:\n"
